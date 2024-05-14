196375580c2671900b14347ec58a65d7090bc180
Hin Yau <99358826+nzxzn@users.noreply.github.com>
2024-05-14 13:48:24 -0700

    Add calculator, caesarCipher, and analyzeArray

--- src/index.js
+++ src/index.js
@@ -13,4 +13,84 @@ const reverseString = (str) => {
   return strArray.join('');
 };
 
-export { capitalize, reverseString };
+const calculator = () => {
+  const add = (a, b) => {
+    if (isNaN(a) || isNaN(b)) throw new Error('Not a valid number!');
+    if (a == '' || b == '') throw new Error('Empty strings are not valid!');
+    return a + b;
+  };
+
+  const subtract = (a, b) => {
+    if (isNaN(a) || isNaN(b)) throw new Error('Not a valid number!');
+    if (a == '' || b == '') throw new Error('Empty strings are not valid!');
+
+    return a - b;
+  };
+
+  const multiply = (a, b) => {
+    if (isNaN(a) || isNaN(b)) throw new Error('Not a valid number!');
+    if (a == '' || b == '') throw new Error('Empty strings are not valid!');
+
+    return a * b;
+  };
+
+  const divide = (a, b) => {
+    if (isNaN(a) || isNaN(b)) throw new Error('Not a valid number!');
+    if (a == '' || b == '') throw new Error('Empty strings are not valid!');
+    if (b == 0) throw new Error('Zero division is illegal');
+
+    return a / b;
+  };
+
+  return { add, subtract, multiply, divide };
+};
+
+const caesarCipher = (str, key) => {
+  const zCode = 'z'.charCodeAt(0);
+  const aCode = 'a'.charCodeAt(0);
+
+  const ZCode = 'Z'.charCodeAt(0);
+  const ACode = 'A'.charCodeAt(0);
+
+  return [...str]
+    .map((char) => {
+      let code = char.charCodeAt(0);
+      if (code <= zCode && code >= aCode) {
+        code += key % 26;
+        code = code > zCode ? code - 26 : code;
+        code = code < aCode ? code + 26 : code;
+        return String.fromCharCode(code);
+      }
+      if (code <= ZCode && code >= ACode) {
+        code += key % 26;
+        code = code > ZCode ? code - 26 : code;
+        code = code < ACode ? code + 26 : code;
+        return String.fromCharCode(code);
+      }
+      return char;
+    })
+    .join('');
+};
+
+const analyzeArray = (arr) => {
+  if (arr.length === 0) throw new Error('This is an empty array!');
+
+  let min = Infinity;
+  let max = -Infinity;
+
+  const average =
+    arr.reduce((accumulator, item) => {
+      if (typeof item !== 'number' || Number.isNaN(item))
+        throw new Error('The items need to be a number');
+
+      min = Math.min(min, item);
+      max = Math.max(max, item);
+
+      return accumulator + item;
+    }, 0) / arr.length;
+
+  const { length } = arr;
+
+  return { average, min, max, length };
+};
+export { capitalize, reverseString, calculator, caesarCipher, analyzeArray };
--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,11 @@
 import { expect, test } from '@jest/globals';
-import { capitalize, reverseString } from './index.js';
+import {
+  capitalize,
+  reverseString,
+  calculator,
+  caesarCipher,
+  analyzeArray,
+} from './index.js';
 
 test('capitalize first letter for normal English words', () => {
   const words = ['hello', 'test', 'divide'];
@@ -49,4 +55,67 @@ test('reverse string', () => {
   strings.forEach((str, index) => {
     expect(reverseString(str)).toBe(answers[index]);
   });
+});
+
+test('add in calculator', () => {
+  const calc = calculator();
+  expect(calc.add(2, 1)).toBe(3);
+  expect(calc.add(4, 6)).toBe(10);
+  expect(() => calc.add('asd', 'vwwef')).toThrow(Error);
+  expect(() => calc.add('', '')).toThrow(Error);
+  expect(() => calc.add('123', '!@%')).toThrow(Error);
+});
+
+test('subtract in calculator', () => {
+  const calc = calculator();
+
+  expect(calc.subtract(3, 1)).toBe(2);
+  expect(calc.subtract(-10, -3.734)).toBeCloseTo(-6.266);
+  expect(() => calc.subtract('asidQ!u', '234')).toThrow(Error);
+  expect(() => calc.subtract('', '')).toThrow(Error);
+});
+
+test('multiply in calculator', () => {
+  const calc = calculator();
+
+  expect(calc.multiply(20, 4)).toBe(80);
+  expect(calc.multiply('12', 4)).toBe(48);
+  expect(() => calc.multiply('fa1', '4')).toThrow(Error);
+  expect(calc.multiply(Infinity, 4)).toBe(Infinity);
+});
+
+test('division in calculator', () => {
+  const calc = calculator();
+
+  expect(calc.divide(10, 2)).toBe(5);
+  expect(calc.divide(10, 3)).toBeCloseTo(3.33, 2);
+  expect(() => calc.divide(123, 0)).toThrow(Error);
+  expect(calc.divide(123, Infinity)).toBe(0);
+  expect(() => calc.divide('asidub', 123)).toThrow(Error);
+});
+
+test('caesarCipher to shift character', () => {
+  expect(caesarCipher('what', 2)).toBe('yjcv');
+  expect(caesarCipher('abcdefghijklmnopqrstuvwxyz', 5)).toBe(
+    'fghijklmnopqrstuvwxyzabcde'
+  );
+  expect(caesarCipher('abcdefghijklmnopqrstuvwxyz', -20)).toBe(
+    'ghijklmnopqrstuvwxyzabcdef'
+  );
+  expect(caesarCipher('How are you?', 2)).toBe('Jqy ctg aqw?');
+  expect(caesarCipher('SIDJFBD@#F346 WOFFISDFQ ADFJSDF', 47)).toBe(
+    'NDYEAWY@#A346 RJAADNYAL VYAENYA'
+  );
+  expect(caesarCipher('', 23)).toBe('');
+});
+
+test('array', () => {
+  const testArray1 = [1, 8, 3, 4, 2, 6];
+  const testArray2 = ['sdfsd', 'asd', '123', 123];
+
+  expect(analyzeArray(testArray1).average).toBe(4);
+  expect(() => analyzeArray(testArray2).average).toThrow(Error);
+  expect(analyzeArray(testArray1).min).toBe(1);
+  expect(analyzeArray(testArray1).length).toBe(6);
+  expect(() => analyzeArray([]).average).toThrow(Error);
 });
